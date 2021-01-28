b3cce1f38677ff3f893ddc8942e2c008044bbe83
David Sauer <davedamoon@gmail.com>
2021-01-28 08:47:39 +0100

    fixed data ownership to be able to read multiple streams, and start stop the processing

--- examples/example.rs
+++ examples/example.rs
@@ -1,10 +1,17 @@
 use tokio::signal;
 
+use rustreams::Mapper;
 use rustreams::Streams;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut streams = Streams::new();
+
+    let input1 = streams.read("input1");
+    let input2 = streams.read("input2");
+    let length1 = input1.map(|s: Option<&[u8]>| -> usize { s.unwrap().len() });
+    let length1 = input1.map(|s: usize| -> &[u8] { i.to_be_bytes() });
+
     streams.start().await;
     signal::ctrl_c().await?;
     streams.stop().await;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(in_band_lifetimes)]
+
 use std::{borrow::Borrow, time::Duration};
 use tokio::{task::JoinHandle, time::sleep};
 
@@ -15,7 +17,7 @@ pub struct Streams<'a> {
     streams: HashMap<&'a str, Sender<Option<&'a [u8]>>>,
 }
 
-struct Stream<V> {
+pub struct Stream<V> {
     rx: Receiver<V>,
 }
 
@@ -29,7 +31,7 @@ impl<'a> Streams<'a> {
         }
     }
 
-    fn read(mut self, topic_name: &'a str) -> Stream<Option<&[u8]>> {
+    pub fn read(&mut self, topic_name: &'a str) -> Stream<Option<&'a [u8]>> {
         let (tx, rx) = channel(1);
         self.streams.insert(topic_name, tx);
         Stream::<Option<&[u8]>> { rx }
@@ -54,16 +56,21 @@ impl<'a> Streams<'a> {
         }));
     }
 
-    pub async fn stop(self) {
-        match (self.task, self.tx) {
-            (Some(task), Some(tx)) => {
-                println!("shutting down");
-                tx.send(()).expect("failed to signal shutdown");
-                task.await.expect("failed to wait for shutdown");
-                println!("shut down complete");
-            }
-            _ => panic!("streams not running"),
+    pub async fn stop(mut self) {
+        if self.task.is_none() || self.tx.is_none() {
+            panic!("streams not running")
         }
+
+        println!("shutting down");
+        self.tx
+            .unwrap()
+            .send(())
+            .expect("failed to signal shutdown");
+        self.task
+            .unwrap()
+            .await
+            .expect("failed to wait for shutdown");
+        println!("shut down complete");
     }
 }
 
@@ -81,5 +88,58 @@ impl<V> Stream<V> {
 
     fn write(mut self, topic_name: &str) {
         // TODO
+    }
+}
+
+pub trait Mapper<V1, V2> {
+    fn map(self, m: impl Send + 'static + Fn(V1) -> V2) -> Stream<V2>;
+}
+
+impl<V1: Send + 'static, V2: Send + 'static> Mapper<V1, V2> for Stream<V1> {
+    fn map(mut self, map: impl Send + 'static + Fn(V1) -> V2) -> Stream<V2> {
+        let (tx, rx) = channel::<V2>(1);
+
+        tokio::spawn(async move {
+            let ten_millis = std::time::Duration::from_millis(10);
+            std::thread::sleep(ten_millis);
+
+            use futures::task::Poll;
+            match self.poll_recv() {
+                Poll::Pending => {
+                    println!("pending")
+                }
+                Poll::Ready(Some(message)) => {
+                    println!("msg");
+                    let new_message = map(message);
+                    if let Err(e) = tx.send(new_message).await {
+                        panic!("failed to send: {}", e);
+                    }
+                }
+                Poll::Ready(None) => {
+                    println!("closed")
+                }
+            };
+
+            while let Some(message) = self.rx.recv().await {
+                let new_message = map(message);
+                if let Err(e) = tx.send(new_message).await {
+                    panic!("failed to send: {}", e);
+                }
+            }
+
+            match self.poll_recv() {
+                Poll::Pending => {
+                    println!("pending")
+                }
+                Poll::Ready(Some(message)) => {
+                    println!("msg")
+                }
+                Poll::Ready(None) => {
+                    println!("closed")
+                }
+            };
+        });
+
+        Stream { rx }
     }
 }
