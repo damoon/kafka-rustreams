2d2cdb9fca31e9d89869b07c0639ac65e408bd30
David Sauer <davedamoon@gmail.com>
2022-02-22 08:26:35 +0100

    keep doing minimum

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ enum StreamWrite<K, V> {
     Write(&'static str, Message<K, V>),
 }
 
-#[derive(Debug, Clone,)]
+#[derive(Debug, Clone)]
 pub struct Message<K, V> {
     pub key: Option<K>,
     pub value: Option<V>,
@@ -46,50 +46,48 @@ impl<K, V> Message<K, V> {
 }
 
 pub struct Topology {
-    inputs: HashMap<&'static str, Vec<Sender<StreamMessage<&'static [u8], &'static [u8]>>>>,
-
+    inputs: HashMap<&'static str, Vec<Sender<StreamMessage<Vec<u8>, Vec<u8>>>>>,
     flush_needed: Arc<AtomicUsize>,
-
-    flushed_tx: Sender<()>,
-    flushed_rx: Receiver<()>,
-
-    writes_tx: Sender<StreamWrite<&'static dyn ToBytes, &'static dyn ToBytes>>,
-    writes_rx: Receiver<StreamWrite<&'static dyn ToBytes, &'static dyn ToBytes>>,
+    writes_tx: Sender<StreamWrite<Vec<u8>, Vec<u8>>>,
+    writes_rx: Receiver<StreamWrite<Vec<u8>, Vec<u8>>>,
 }
 
 impl Default for Topology {
     fn default() -> Self {
-            let inputs = HashMap::new();
-            let flush_needed = Arc::new(AtomicUsize::new(0));
-            let (flushed_tx, flushed_rx) = channel(1);
-            let (writes_tx, writes_rx) = channel(CHANNEL_BUFFER_SIZE);
-    
-            Topology {
-                inputs,
-                flush_needed,
-                flushed_tx,
-                flushed_rx,
-                writes_tx,
-                writes_rx,
-            }
+        let inputs = HashMap::new();
+        let flush_needed = Arc::new(AtomicUsize::new(0));
+        let (writes_tx, writes_rx) = channel(CHANNEL_BUFFER_SIZE);
+
+        Topology {
+            inputs,
+            flush_needed,
+            writes_tx,
+            writes_rx,
+        }
     }
 }
 
 impl Topology {
-    pub fn read_from<K: FromBytes<Error = &'static dyn Debug>, V: FromBytes<Error = &'static dyn Debug>>(&mut self, topic: &'static str) -> Stream<K, V> {
-        let (mut tx, ri) = channel(CHANNEL_BUFFER_SIZE);
+    pub fn read_from<
+        K: 'static + Send + Debug + Clone + FromBytes<Error = &'static dyn Debug>,
+        V: 'static + Send + Debug + Clone + FromBytes<Error = &'static dyn Debug>,
+    >(
+        &mut self,
+        topic: &'static str,
+    ) -> Stream<K, V> {
+        let (tx, mut ri) = channel(CHANNEL_BUFFER_SIZE);
 
         match self.inputs.get_mut(topic) {
             None => {
                 let subscriptions = vec![tx];
                 self.inputs.insert(topic, subscriptions);
-            },
+            }
             Some(subscriptions) => {
                 subscriptions.push(tx);
-            },
+            }
         };
 
-        let (mut ti, rx) = channel::<StreamMessage<K, V>>(CHANNEL_BUFFER_SIZE);
+        let (ti, rx) = channel::<StreamMessage<K, V>>(CHANNEL_BUFFER_SIZE);
 
         tokio::spawn(async move {
             // until shutting down
@@ -97,26 +95,31 @@ impl Topology {
                 match m {
                     StreamMessage::Flush => {
                         ti.send(StreamMessage::Flush)
-                        .await
-                        .expect("failed to forward flush message");
-                    },
+                            .await
+                            .expect("failed to forward flush message");
+                    }
                     StreamMessage::Message(m) => {
-                        let key = m.key
-                        .map(K::from_bytes)
-                        .map(|v| v.expect("key failed to decode"));
-                        let value = m.value
-                        .map(V::from_bytes)
-                        .map(|v| v.expect("value failed to decode"));
+                        let key = m
+                            .key
+                            .as_deref()
+                            .map(K::from_bytes)
+                            .map(|v| v.expect("key failed to decode"))
+                            .cloned();
+                        let value = m
+                            .value
+                            .as_deref()
+                            .map(V::from_bytes)
+                            .map(|v| v.expect("value failed to decode"))
+                            .cloned();
 
-
-                        ti.send(StreamMessage::Message(Message{
+                        ti.send(StreamMessage::Message(Message {
                             key,
                             value,
                             timestamp: m.timestamp,
                         }))
                         .await
                         .expect("failed to send decoded message");
-                    },
+                    }
                 }
             }
         });
@@ -129,77 +132,85 @@ impl Topology {
         }
     }
 
-    async fn flush(&mut self
-        //         inputs: HashMap<&str, Sender<StreamMessage<Key, Value>>>,
-        //         flush_needed: Arc<AtomicUsize>,
-        //         flushed_rx: &mut Receiver<()>,
-            ) {
-                log::debug!("flushing");
+    async fn flush(
+        &mut self, //         inputs: HashMap<&str, Sender<StreamMessage<Key, Value>>>,
+                   //         flush_needed: Arc<AtomicUsize>,
+                   //         flushed_rx: &mut Receiver<()>
+    ) {
+        log::debug!("flushing");
 
-                let mut flush_signals = 0;
+        let mut flush_signals = 0;
 
-                for inputs in self.inputs.iter() {
-                    log::debug!("request flush");
+        for inputs in self.inputs.iter() {
+            log::debug!("request flush");
 
-                    for input in inputs.1.iter() {
-                        flush_signals += 1;
-                        input
-                            .send(StreamMessage::Flush)
-                            .await
-                            .expect("failed to trigger flush");
-                    }
-            
-                    log::debug!("requested flush");
-                }
-
-                let expected_acks = flush_signals * self.flush_needed.load(Ordering::Relaxed);
-            
-                log::debug!("await {} flushes", expected_acks);
-
-                for _ in 0..expected_acks {
-                    log::debug!("await flush ack");
-            
-                    self.flushed_rx
-                        .recv()
-                        .await
-                        .expect("failed to receive flush acknowledge");
-                }
-            
-                log::debug!("all flushes acked");
+            for input in inputs.1.iter() {
+                flush_signals += 1;
+                input
+                    .send(StreamMessage::Flush)
+                    .await
+                    .expect("failed to trigger flush");
             }
+
+            log::debug!("requested flush");
+        }
+
+        let expected_acks = flush_signals * self.flush_needed.load(Ordering::Relaxed);
+
+        log::debug!("await {} flushes", expected_acks);
+
+        for _ in 0..expected_acks {
+            log::debug!("await flush ack");
+
+            self.flushed_rx
+                .recv()
+                .await
+                .expect("failed to receive flush acknowledge");
+        }
+
+        log::debug!("all flushes acked");
+    }
 }
 
 pub struct Stream<K, V> {
     rx: Receiver<StreamMessage<K, V>>,
-    writes: Sender<StreamWrite<&'static dyn ToBytes, &'static dyn ToBytes>>,
+    writes: Sender<StreamWrite<Vec<u8>, Vec<u8>>>,
     flush_needed: Arc<AtomicUsize>,
     //flushed: Sender<()>,
 }
 
-impl<P: FromBytes> Stream<P, P> {
+impl<K: 'static + Send + Debug + Clone + ToBytes, V: 'static + Send + Debug + Clone + ToBytes>
+    Stream<K, V>
+{
     pub fn write_to(mut self, topic: &'static str) {
         self.flush_needed.fetch_add(1, Ordering::Relaxed);
 
         tokio::spawn(async move {
-            loop {
-                match self.rx.recv().await {
-                    Some(StreamMessage::Message(message)) => {
+            while let Some(m) = self.rx.recv().await {
+                match m {
+                    StreamMessage::Flush => {
                         self.writes
-                            .send(StreamWrite::Write(topic, message))
+                            .send(StreamWrite::Flush)
                             .await
-                            .expect("failed to forward message");
+                            .expect("failed to forward flush message");
                     }
-                    Some(StreamMessage::Flush) => {
-                        if let Err(e) = self.writes.send(StreamWrite::Flush).await {
-                            panic!("failed to forward flush: {}", e);
-                        }
-                    }
-                    None => {
-                        log::debug!("write to topic thread stopped");
+                    StreamMessage::Message(m) => {
+                        let key = m.key.map(|v| v.to_bytes().to_vec());
+                        let value = m.value.map(|v| v.to_bytes().to_vec());
 
-                        return;
+                        self.writes
+                            .send(StreamWrite::Write(
+                                topic,
+                                Message {
+                                    key,
+                                    value,
+                                    timestamp: m.timestamp,
+                                },
+                            ))
+                            .await
+                            .expect("failed to send encoded message");
                     }
-                };
+                }
             }
         });
     }
